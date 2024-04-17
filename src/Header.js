62939e86d9cd90542000c61038ec37ab57c06406
Colleen Evans <136169419+CherryishUncertainty@users.noreply.github.com>
2024-04-17 12:32:02 +0800

    fixed nav bar component and added meta tags.

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,3 @@
 <header>Homepage
     <img></img>
-    <nav>
-        <ul>
-            <li><a href="#"></a>Home</li>
-            <li><a href="#"></a>About</li>
-            <li><a href="#"></a>Menu</li>
-            <li><a href="#"></a>"Reservations</li>
-            <li><a href="#"></a>Order Online</li>
-            <li><a href='#'></a>'Login</li>
-        </ul>
-    </nav>
 </header>
